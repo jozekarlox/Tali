fe67e642c53f9ab11a65faccab34fde2852400e7
thalesvinicius.26@gmail.com <thalesvinicius.26@gmail.com>
2019-04-18 13:31:31 -0300

    Adição da classe template para manipulação de html

--- php/public/index.php
+++ php/public/index.php
@@ -1,21 +1,18 @@
 <?php
 define('CORE_DIR', dirname(__FILE__, 2).DIRECTORY_SEPARATOR.'src'.DIRECTORY_SEPARATOR.'Core'.DIRECTORY_SEPARATOR);
 define('CTRL_DIR', dirname(__FILE__, 2).DIRECTORY_SEPARATOR.'src'.DIRECTORY_SEPARATOR.'App'.DIRECTORY_SEPARATOR.'Controller'.DIRECTORY_SEPARATOR);
+define('VIEW_DIR', dirname(__FILE__, 2).DIRECTORY_SEPARATOR.'src'.DIRECTORY_SEPARATOR.'App'.DIRECTORY_SEPARATOR.'View'.DIRECTORY_SEPARATOR);
 
 spl_autoload_register(function ($class_name) {
     $file = CORE_DIR . $class_name . '.php';
     if ( !is_file($file) ) $file = CTRL_DIR . $class_name . '.php';
+    if ( !is_file($file) ) $file = VIEW_DIR . $class_name . '.php';
     include $file; 
 });
 
-// require_once(dirname(__FILE__, 2) . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'Core' . DIRECTORY_SEPARATOR . 'Router.php');
-
 $router = new Router();
 
-$router->on('GET', '/', function () { 
-    $loader = new Loader();
-    $loader->homePage();
-});
+$router->on('GET', '/', function () { $loader = new Loader(); $loader->homePage(); });
 
 // $router->on('GET', '/home', 'DevMania\App\Controller\Dashboard->loadPage');
 
--- php/src/App/Controller/Loader.php
+++ php/src/App/Controller/Loader.php
@@ -2,8 +2,15 @@
 
 class Loader {
 
+    private $template;
+
+    public function __construct() {
+        $this->template = new Template();
+    }
+
     public function homePage() {
-        echo 'Hello World02';
+        $this->template->setFile('teste01');
+        die( $this->template->getFile() );
     }
 }
 
